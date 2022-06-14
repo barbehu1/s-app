72b6f63887f20e27da1925ba0c4052383d3ba780
barbehu1 <barbehugo28@gmail.com>
2022-06-14 21:59:51 +0200

    Ajout du lien de la font Nunito

--- src/footer.jsx
+++ src/footer.jsx
@@ -11,35 +11,35 @@ import cercle from './images/circle.svg';
 function FooterMobile() {
     return(
         <footer>
-            <img class="circle1" src={cercle} alt="Cercle d'illustration"/>
-            <div class="logoDiv">
-                <img class="logo" src={logoSapp} alt="S'APP"/>
+            <img className="circle1" src={cercle} alt="Cercle d'illustration"/>
+            <div className="logoDiv">
+                <img className="logo" src={logoSapp} alt="S'APP"/>
             </div>
-            <div class="downloadButtonDiv">
+            <div className="downloadButtonDiv">
                 <button>Télécharger</button>
             </div>
-            <div class="divProjet">
+            <div className="divProjet">
                 <h3>PROJET</h3>
                 <ul>
                     <li>Équipe</li>
                     <li>Indépendance</li>
                 </ul>
             </div>
-            <div class="divApplication">
+            <div className="divApplication">
                 <h3>APPLICATION</h3>
                 <ul>
                     <li>Features</li>
                     <li>FAQ</li>
                 </ul>
             </div>
-            <div class="divLiens">
+            <div className="divLiens">
                 <h3>LIENS UTILES</h3>
                 <ul>
                     <a href="mailto:hugo.barbe@edu.devinci.fr"><li>Contactez-nous</li></a>
                     <li>Mentions légales</li>
                 </ul>
             </div>
-            <img class="circle2" src={cercle} alt="Cercle d'illustration"/>
+            <img className="circle2" src={cercle} alt="Cercle d'illustration"/>
         </footer>
     );
 }
